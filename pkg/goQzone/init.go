800f31964e5f09a38781c8e98ed6bd649b7d1dd9
杨成锴 <243852814@qq.com>
2021-08-05 11:36:43 +0800

    remove proxy

--- pkg/goQzone/init.go
+++ pkg/goQzone/init.go
@@ -16,7 +16,7 @@ func Init() *Service {
 	s.Request = gorequest.New()
 	s.Request.DoNotClearSuperAgent = true
 	s.Request.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36")
-	s.Request.Proxy("http://127.0.0.1:8866")
+	//s.Request.Proxy("http://127.0.0.1:8866")
 	go s.keepCookieAlive()
 	return s
 }
